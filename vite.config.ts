60a9da10208a8e5ecaaf9dcb496537cb8aa8b460
RASHI BHATT <rashibhatt@uniqus.com>
2024-04-22 21:41:28 +0530

    updated vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-dotenv.config();
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // define process env
   define: {
-    'process.env': process.env
-  }
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 79abca296f424cddbedb4237ab2e04f9804e1213
+    'process.env': process.env,
+  },
 })
